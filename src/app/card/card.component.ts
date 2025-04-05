b132aeab707bc5ae322efab97d2e61cdf71fd253
Marcello <cellolim4jpa@gmail.com>
2025-04-05 12:09:34 -0300

    Propriedade template

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  templateUrl: './card.component.html',
+  //templateUrl: './card.component.html',
+    template: `<div class="card">
+                  <div class="card__plan card__item">Plano <b>teste</b></div>
+                  <div class="card__price card__item">R$ {{plano.valor}}</div>
+                  <app-card-button></app-card-button>
+                </div>`,
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
